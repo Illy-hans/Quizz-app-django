e3f462acc0b368f922b180c1e435bf44b77309e7
ilhan <ilhanabdalle@gmail.com>
2024-04-29 19:16:09 +0100

    add model updates

--- quizzordy/serializer.py
+++ quizzordy/serializer.py
@@ -2,6 +2,7 @@ from django.utils.safestring import mark_safe
 from rest_framework import serializers
 from .models import QuestionsDB, Quizzes
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 class QuestionsDBSerializer(serializers.ModelSerializer):  
     class Meta:
@@ -19,3 +20,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta: 
         model = User
         fields = ["id", "password", "username","first_name", "last_name", "email", "date_joined"]
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect details")
--- quizzordy/views.py
+++ quizzordy/views.py
@@ -1,10 +1,13 @@
 from rest_framework import generics, status
-from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from django.db.models.functions import Random
 import requests
 from django.http import JsonResponse
 from django.contrib.auth.hashers import make_password
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.exceptions import MethodNotAllowed
 
 from .models import QuestionsDB
 from .serializer import QuestionsDBSerializer
@@ -14,6 +17,7 @@ from .serializer import QuizzesSerializer
 
 from django.contrib.auth.models import User
 from .serializer import UserSerializer
+from .serializer import UserLoginSerializer
 
 # QUESTIONS
 # This just needs to list 
@@ -26,7 +30,6 @@ class QuestionsDBListCreate(generics.ListCreateAPIView):
         serializer = self.serializer_class(random_questions, many=True)
         return JsonResponse({"questions": serializer.data, "status": "200"})
 
-
     # delete function for developement
     def delete(self, request, *arg, **kwargs):
         QuestionsDB.objects.all().delete()
@@ -37,9 +40,11 @@ class QuestionsDBRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = QuestionsDBSerializer
     lookup_field = "pk"
 
-def add(request):
+class AddQuestionsAPIView(GenericAPIView):
+
+    def get(self, request, *args, **kwargs):
         url = "https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple"
-        response = requests.get(url)
+        response = requests.get(url, timeout=10)
         if response.status_code == 200:
             data = response.json()
             for result in data['results']: 
@@ -82,13 +87,16 @@ class UserListCreate(generics.ListCreateAPIView):
     serializer_class = UserSerializer
 
     # overrides the default create method to include password hashing 
-    def create(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
+        permission_classes = (AllowAny)
+        data = {}
         try:
             # data is copied so the password can be amended and then saved
             request_data_copy = request.data.copy()
             email = request_data_copy.get('email')
             if User.objects.filter(email=email).exists():
-                return JsonResponse({"status": 400, "message": "Email is already in use"})
+                data["message"] = "Email is already in use."
+                return JsonResponse(data, status=status.HTTP_400_BAD_REQUEST)
             
             else: 
                 password = request_data_copy.get('password')
@@ -98,12 +106,16 @@ class UserListCreate(generics.ListCreateAPIView):
                     # Set the hashed password in the request data
                     request_data_copy['password'] = hashed_password
 
-                    # Call the create method
                     serializer = self.get_serializer(data=request_data_copy)
                     serializer.is_valid(raise_exception=True)
-                    self.perform_create(serializer)
+                    user = serializer.save()
+                    token = RefreshToken.for_user(user)
+                    data = serializer.data
+                    data["tokens"] = {"refresh": str(token),
+                                    "access": str(token.access_token)}
+                    data["message"] = "User has been created"
 
-                    return JsonResponse({"status": 201, "message": "User added successfully"})
+                    return JsonResponse(data, status=status.HTTP_201_CREATED)
 
         except Exception as e:
             return JsonResponse({"status": 500, "message": f"Error: {e}"})
@@ -121,3 +133,33 @@ class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = "pk"
 
 
+class UserLoginAPIView(GenericAPIView):
+    permission_classes = (AllowAny),
+    serializer_class = UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        serializer = UserSerializer(user)
+        token = RefreshToken.for_user(user)
+        data = serializer.data
+        data["tokens"] = {"refresh": str(token),
+                        "access": str(token.access_token)}
+        
+        return JsonResponse(data, status=status.HTTP_200_OK)
+
+
+class UserLogoutAPIView(GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token= request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return JsonResponse(status=status.HTTP_205_RESET_CONTENT)
+        
+        except Exception as e:
+            return JsonResponse(status=status.HTTP_400_BAD_REQUEST)
+    
